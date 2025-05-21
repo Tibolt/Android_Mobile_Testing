44bc145cb48afc58aca7d6a8be9464c985d3ec41
Tibolt <tibo.ak015@gmail.com>
2025-05-21 11:12:52 +0200

    add checkTime test

--- src/main/java/org/example/ClockTest.java
+++ src/main/java/org/example/ClockTest.java
@@ -1,14 +1,13 @@
 package org.example;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.net.URL;
 import java.time.Duration;
+import java.time.LocalTime;
 
 public class ClockTest {
     private AndroidDriver driver;
@@ -18,45 +17,47 @@ public class ClockTest {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
         caps.setCapability("appium:automationName","UiAutomator2");
-        caps.setCapability("appium:deviceName","Pixel_5");
-//        caps.setCapability("appium:appPackage", "com.google.android.deskclock");
-//        caps.setCapability("appium:appActivity", "com.android.deskclock.DeskClock");
+        caps.setCapability("appium:deviceName","Pixel_2");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
+
     @BeforeEach
     public void runClock() {
+        driver.terminateApp("com.google.android.deskclock");
         driver.activateApp("com.google.android.deskclock");
     }
 
     @Test
-    public void openTimerTab() {
-        driver.findElement(By.id("com.google.android.deskclock:id/digital_clock")).click();
-        driver.findElement(By.xpath("//android.widget.TextView[@text='Timer']")).click();
+    public void checkTime() {
+        driver.findElement(By.id("com.google.android.deskclock:id/digital_clock"));
+        String appTime = driver.findElement(By.id("com.google.android.deskclock:id/digital_clock")).getText().trim();
+
+        LocalTime systemTime = LocalTime.now().withSecond(0).withNano(0);
+
+        System.out.println("Clock time: " + appTime);
+        System.out.println("Local system time: " + systemTime);
+
+        Assertions.assertEquals(systemTime, appTime, "App clock time does not match local system time.");
     }
 
     @Test
     public void activateTimer() throws InterruptedException {
-        Actions action = new Actions(driver);
-        action.sendKeys("30").perform();
+        driver.findElement(By.xpath("//android.widget.TextView[@text='TIMER']")).click();
+
+        driver.findElement(By.id("com.google.android.deskclock:id/right_button")).click();
+        driver.findElement(By.id("com.google.android.deskclock:id/timer_setup_digit_1")).click();
+        driver.findElement(By.id("com.google.android.deskclock:id/timer_setup_digit_0")).click();
         driver.findElement(By.id("com.google.android.deskclock:id/fab")).click();
 
-        Thread.sleep(5000); // wait 5 seconds
-        String timeLeft = driver.findElement(By.id("com.google.android.deskclock:id/countdown")).getText();
+        String timeLeft = driver.findElement(By.id("com.google.android.deskclock:id/timer_time_text")).getText();
         System.out.println("Time left: " + timeLeft);
-        Assertions.assertEquals(25, timeLeft);
-    }
+//        Assertions.assertEquals(5, timeLeft);
 
-    @Test
-    public void disableTimer() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.Button[@text='Stop']"))).click();
-    }
-
-    @AfterEach
-    public void shutdownClock() {
-        driver.terminateApp("com.google.android.deskclock");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.Toast[contains(@text, 'Timer')]")));
+        driver.findElement(By.id("android:id/button1")).click();
     }
 
     @AfterEach
